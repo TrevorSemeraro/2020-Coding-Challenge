f6dab2ac9f9dfa74f3514f8297fa0cf7b5dee550
Trevor Semeraro <tsemeraro05@gmail.com>
2025-07-17 18:16:26 -0400

    Trevor Semeraro ts3681

--- server.py
+++ server.py
@@ -51,12 +51,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    scoreboard = sorted(scoreboard, key=lambda x: x['score'], reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
-
-
